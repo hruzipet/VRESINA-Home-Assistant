460172eec5c5c99469c47732cb58e5cea84cc885
root <petr@hruzik.net>
2022-11-04 09:50:13 +0100

    added mqtt docker

---
+++ esphome/.esphome/build/esp32-vresina-1p/src/esphome/core/defines.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "esphome/core/macros.h"
+#define ESPHOME_BOARD "esp32cam"
+#define ESPHOME_VARIANT "ESP32"
+#define USE_API
+#define USE_API_PLAINTEXT
+#define USE_ARDUINO_VERSION_CODE VERSION_CODE(1, 0, 6)
+#define USE_BLUETOOTH_PROXY
+#define USE_BUTTON
+#define USE_CAPTIVE_PORTAL
+#define USE_DASHBOARD_IMPORT
+#define USE_JSON
+#define USE_LOGGER
+#define USE_MDNS
+#define USE_OTA
+#define USE_OTA_PASSWORD
+#define USE_OTA_STATE_CALLBACK
+#define USE_SENSOR
+#define USE_SOCKET_IMPL_BSD_SOCKETS
+#define USE_WEBSERVER
+#define USE_WEBSERVER_PORT 80
+#define USE_WEBSERVER_VERSION 2
+#define USE_WIFI
